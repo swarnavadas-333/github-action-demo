3c30dc4f0f8e70c2cb6b78d0acb9f1cc09eae118
swarnavadas-333 <swarnavadas.333@gmail.com>
2024-10-15 01:53:15 +0530

    sub func added

--- src/addition.py
+++ src/addition.py
@@ -1,6 +1,14 @@
 def add(a, b):
     return a+b
 
+def sub(a, b):
+    return a-b
+
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+
+def test_sub():
+    assert sub(2, 1) == 1
+    assert sub(5, 2) == 3
+
